098ef32836d41697e312a31c9cfc030dada99927
JaouadOukassi <jaouadoukassi1@gmail.com>
2022-11-22 07:36:53 +0100

    Subir todo

---
+++ js/main.js
@@ -0,0 +1,15 @@
+// scrollHeight: Entire Content & Padding (Visible or Not)
+// clientHeight: visible Content & Padding
+
+let el = document.querySelector('.scroller');
+let height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+
+console.log(document.documentElement.scrollHeight )
+console.log(document.documentElement.clientHeight)
+console.log(height)
+
+
+window.addEventListener('scroll', () =>{
+    let scrollTop = document.documentElement.scrollTop;
+    el.style.width = `${(scrollTop / height)*100}%`;
+});
